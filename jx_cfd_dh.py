f6d38b07075107eac1dc120f34fac8efb521558c
2371643948 <60834272+2371643948@users.noreply.github.com>
2022-05-29 02:32:23 +0800

    Update jx_cfd_dh.py

--- jx_cfd_dh.py
+++ jx_cfd_dh.py
@@ -16,7 +16,7 @@ cfd_start_time = -0.15
 cfd_offset_time = 0.01
 
 # 基础配置勿动
-cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&bizCode=jxbfd&source=jxbfd&dwEnv=7&_cfd_t=1648778416388&ptag=7155.9.47&dwType=3&dwLvl=2&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202204&strPgtimestamp=1648778416345&strPhoneID=1ef0eafd7b25011a040a37ab925a5586ebfe3f35&strPgUUNum=1674e9a767125864a580ba003b8e655b&_stk=_cfd_t%2CbizCode%2CddwPaperMoney%2CdwEnv%2CdwLvl%2CdwType%2Cptag%2Csource%2CstrPgUUNum%2CstrPgtimestamp%2CstrPhoneID%2CstrPoolName%2CstrZone&_ste=1&h5st=20220401100016388%3B9785369927142102%3B92a36%3Btk02w5c951a1218nwt7hCp33UXI2Fjh60u3JfB%2BJ8MEWiMV%2Fibq4jLJPa3oZjNWJAHB1M35JEoTGURFBUSdW9vmWI3Y4%3B62cd48fa4161a161d28b0491ea99225665b121721a11208c5ffe19b0cf1cf4c7%3B3.0%3B1648778416388&_=1648778416389&sceneval=2&g_login_type=1&callback=jsonpCBKX&g_ty=ls"
+cfd_url = "https://m.jingxi.com/jxbfd/user/ExchangePrize?strZone=jxbfd&dwType=3&dwLvl=1&ddwPaperMoney=100000&strPoolName=jxcfd2_exchange_hb_202205&sceneval=2&g_login_type=1"
 pattern_pin = re.compile(r'pt_pin=([\w\W]*?);')
 pattern_data = re.compile(r'\(([\w\W]*?)\)')
 
@@ -80,15 +80,17 @@ def cfd_qq(def_start_time):
     d1 = datetime.datetime.now().strftime("%H:%M:%S.%f")
     res = requests.get(cfd_url, headers=headers)
     t2 = time.time()
-    # 正则对结果进行提取
-    re_list = pattern_data.search(res.text)
     # 进行json转换
-    data = json.loads(re_list.group(1))
+    data = json.loads(res.text)
     msg = data['sErrMsg']
     # 根据返回值判断
     if data['iRet'] == 0:
         # 抢到了
         msg = "可能抢到了"
+        put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
+        disable_env(u_cookie.get('_id'))
+    elif data['iRet'] == 1023:
+        # 信号弱
         put_envs(u_cookie.get('_id'), u_cookie.get('name'), u_cookie.get('value'), msg)
         disable_env(u_cookie.get('_id'))
     elif data['iRet'] == 2016:
